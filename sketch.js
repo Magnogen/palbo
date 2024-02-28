1c94bb67147a75ab646fd14a88a0b2725328adbe
Magnogen <hydronesium@gmail.com>
2024-02-28 15:56:23 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,218 @@
+// jshint esversion: 11, loopfunc: true
+(async () => {
+
+    const loadData = () => {
+        let data = JSON.parse(localStorage.getItem('data')) ?? {
+            palettes: [],
+            data: {}
+        };
+        let parsed = { palettes: [...data.palettes], data: {} };
+        for (const palette of data.palettes) {
+            parsed.data[palette] = [];
+            for (const hexCode of data.data[palette]) {
+                parsed.data[palette].push(chroma(hexCode));
+            }
+        }
+        return parsed;
+    };
+
+    const saveData = (data) => {
+        const parsed = { data: {} };
+        parsed.palettes = [...data.palettes];
+
+        for (const palette of data.palettes) {
+            parsed.data[palette] = [];
+            for (const colour of data.data[palette]) {
+                parsed.data[palette].push(colour.hex());
+            }
+        }
+        localStorage.setItem('data', JSON.stringify(parsed));
+    };
+
+    data = loadData();
+
+    let addPaletteBtn = $('#add-palette');
+    let addColourBtn = $('#add-colour');
+    let allColours = $('main');
+    let editor = $('#editor');
+    let editingColour = $('#editing');
+    let originalColour = $('#editing-original');
+    editor.style.pointerEvents = 'none';
+    editor.style.opacity = '0';
+
+    let selectedPalette;
+
+    addPaletteBtn.style.width = getComputedStyle(addPaletteBtn).getPropertyValue('height');
+
+    const makePalette = (name) => {
+        data.palettes.push(name);
+        data.data[name] = [];
+        saveData(data);
+    };
+
+    const createPaletteElement = (name) => {
+        let element = document.createElement('div');
+        element.classList.add('palette');
+        element.innerText = name;
+        element.on('click', () => selectPalette(element));
+        addPaletteBtn.insertAdjacentElement('beforebegin', element);
+        return element;
+    };
+
+    const renamePalette = (oldName, newName) => {
+        if (newName == oldName) return;
+        data.palettes[data.palettes.indexOf(oldName)] = newName;
+
+        data.data[newName] = [...data.data[oldName]];
+        delete data.data[oldName];
+    };
+
+    const getWidthOfInput = (input) => {
+        const tmp = document.createElement("span");
+        tmp.className = "input-element tmp-element";
+        tmp.innerHTML = input.value.replaceAll(/[^A-Za-z0-9_\-]/g, '_');
+        document.body.appendChild(tmp);
+        const theWidth = tmp.getBoundingClientRect().width;
+        document.body.removeChild(tmp);
+        return theWidth;
+    }
+
+    const selectPalette = (element) => {
+        if (!element) return;
+        if (selectedPalette == element) return;
+        if (selectedPalette) {
+            selectedPalette.classList.remove('selected');
+            selectedPalette.innerHTML = selectedPalette.firstChild.value;
+        }
+
+        const input = document.createElement('input');
+        let original = input.value = element.textContent;
+
+        input.on('input', (event) => input.trigger('keydown', event));
+        input.on('keydown', (event) => {
+            if (event.key == 'Enter') input.blur();
+            input.value = input.value.replaceAll(/[^A-Za-z0-9_\-]/g, '_');
+            input.style.width = getWidthOfInput(input) + "px";
+        }); input.trigger('keydown');
+        input.on('blur', (event) => {
+            if (input.value == '') {
+                data.palettes.splice(data.palettes.indexOf(original), 1);
+                delete data.data[original];
+                selectPalette(element.previousElementSibling);
+                element.remove();
+                saveData(data);
+                return;
+            }
+            if (input.value == original) return;
+            if (data.palettes.includes(input.value)) {
+                let number = 1, newValue;
+                do {
+                    newValue = `${input.value}_${++number}`;
+                    if (newValue == original) break;
+                } while (data.palettes.includes(newValue));
+                input.value = newValue;
+                input.trigger('keydown');
+            }
+            renamePalette(original, input.value);
+            original = input.value;
+            saveData(data);
+        });
+
+        element.replaceChildren(input);
+        element.classList.add('selected');
+        selectedPalette = element;
+        // input.select();
+
+        allColours.replaceChildren(addColourBtn);
+        for (const colour of data.data[element.firstChild.value]) {
+            createColourElement(colour);
+        }
+    };
+
+    const makeColour = (colour, paletteElement) => {
+        let palette = paletteElement.innerText;
+        if (paletteElement == selectedPalette) palette = paletteElement.firstChild.value;
+        data.data[palette].push(colour);
+        saveData(data);
+    };
+
+    const createColourElement = (colour) => {
+        let element = document.createElement('div');
+        element.classList.add('colour');
+        element.style.background = colour.css();
+        element.style.color = colour.luminance() > 0.178 ? '#000' : '#fff';
+        element.innerText = colour.hex().substring(1);
+        element.on('click', () => editColour(element));
+        addColourBtn.insertAdjacentElement('beforebegin', element);
+        return element;
+    };
+
+    const editColour = (element) => {
+        editor.style.pointerEvents = 'auto';
+        editor.style.opacity = '1';
+        originalColour.style.background = editingColour.style.background = element.style.background;
+
+        const input = document.createElement('input');
+        let original = input.value = element.textContent;
+        const save = document.createElement('div');
+        save.id = 'save';
+        save.innerHTML = 'save';
+
+        input.style.fontWeight = '400';
+
+        input.on('input', (event) => input.trigger('keydown', event));
+        input.on('keydown', (event) => {
+            if (event.key == 'Enter') { input.blur(); save.click(); }
+            input.value = input.value.toLowerCase().replaceAll(/[^0-9a-f]/g, '');
+            input.style.width = getWidthOfInput(input) + "px";
+            if (input.value.length != 6) return;
+            editingColour.style.background = chroma(input.value).css();
+            if (input.value == original) return;
+            save.classList.add('changed');
+        }); input.trigger('keydown');
+        input.on('blur', (event) => {
+            if (input.value == '') input.value = original;
+            if (input.value == original) return;
+        });
+        save.on('click', (event) => {
+            element.style.background = originalColour.style.background = '#' + input.value;
+            element.textContent = input.value;
+
+            const currentPalette = selectedPalette.firstChild.value;
+            data.data[currentPalette][[].indexOf.call(element.parentNode.children, element)] = chroma(input.value);
+            saveData(data);
+
+            editor.style.opacity = '0';
+            editor.style.pointerEvents = 'none';
+            editor.replaceChildren(editingColour);
+        });
+
+        editor.appendChild(input);
+        editor.appendChild(save);
+    };
+
+    addPaletteBtn.on('click', () => {
+        let id = '';
+        for (let i = 0; i < 4; i++) {
+            id += choose([...'qwrtypsdfghjklzxcvbnm']);
+            id += choose([...'euioa']);
+        }
+        makePalette(id);
+        selectPalette(createPaletteElement(id));
+    });
+
+    addColourBtn.on('click', () => {
+        const colour = chroma.random();
+        makeColour(colour, selectedPalette);
+        createColourElement(colour);
+    });
+
+    let first = true;
+    for (const name of data.palettes) {
+        const elem = createPaletteElement(name);
+        if (first) selectPalette(elem);
+        first = false;
+    }
+
+
+})();
